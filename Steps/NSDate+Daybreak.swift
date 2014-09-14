38580160160759a0d7711cbf819472720809c62d
Adam Binsz <Adam@Adams-MacBook-Air.local>
2014-09-14 16:07:45 -0400

    2.1 submission

---
+++ Steps/NSDate+Daybreak.swift
@@ -0,0 +1,194 @@
+//
+//  NSDate+Daybreak.swift
+//
+// The MIT License (MIT)
+//
+// Copyright (c) 2014 Adam Binsz
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+import Foundation
+
+extension NSDate {
+    
+    // MARK: Days
+    
+    class func beginningOfToday() -> NSDate {
+        return NSDate.date().beginningOfDay()
+    }
+    
+    class func endOfToday() -> NSDate {
+        return NSDate.date().endOfDay()
+    }
+    
+    class func dateDaysFromNow(days: Int) -> NSDate {
+        return NSDate.date().addDays(days)
+    }
+    
+    func beginningOfDay() -> NSDate {
+        let calendar = NSCalendar.currentCalendar()
+        
+        let components = calendar.components(.YearCalendarUnit | .MonthCalendarUnit | .DayCalendarUnit, fromDate: self)
+        
+        return calendar.dateFromComponents(components)!
+    }
+    
+    func endOfDay() -> NSDate {
+        let calendar = NSCalendar.currentCalendar()
+        
+        let components = NSDateComponents()
+        components.day = 1
+        
+        return calendar.dateByAddingComponents(components, toDate: self.beginningOfDay(), options: nil)!.dateByAddingTimeInterval(-1)
+    }
+    
+    func addDays(days: Int) -> NSDate {
+        let calendar = NSCalendar.currentCalendar()
+        
+        let components = NSDateComponents()
+        components.day = days
+        
+        return calendar.dateByAddingComponents(components, toDate: self, options: nil)!
+    }
+    
+    
+    // MARK: Weeks
+    
+    class func beginningOfThisWeek() -> NSDate {
+        return NSDate.date().beginningOfWeek()
+    }
+    
+    class func endOfThisWeek() -> NSDate {
+        return NSDate.date().endOfWeek()
+    }
+    
+    class func dateWeeksFromNow(weeks: Int) -> NSDate {
+        return NSDate.date().addWeeks(weeks)
+    }
+    
+    func beginningOfWeek() -> NSDate {
+        let calendar = NSCalendar.currentCalendar()
+        
+        let components = calendar.components(.YearCalendarUnit | .MonthCalendarUnit | .DayCalendarUnit | .WeekdayCalendarUnit, fromDate: self)
+        
+        let daysToBeginningOfWeek = components.weekday - calendar.firstWeekday
+        components.day -= daysToBeginningOfWeek
+        
+        return calendar.dateFromComponents(components)!
+    }
+    
+    func endOfWeek() -> NSDate {
+        let calendar = NSCalendar.currentCalendar()
+        
+        let components = NSDateComponents()
+        components.weekOfMonth = 1
+        
+        return calendar.dateByAddingComponents(components, toDate: self.beginningOfWeek(), options: nil)!.dateByAddingTimeInterval(-1)
+    }
+    
+    func addWeeks(weeks: Int) -> NSDate {
+        let calendar = NSCalendar.currentCalendar()
+        
+        let components = NSDateComponents()
+        components.weekOfYear = weeks
+        
+        return calendar.dateByAddingComponents(components, toDate: self, options: nil)!
+    }
+    
+    
+    // MARK: Months
+    
+    class func beginningOfThisMonth() -> NSDate {
+        return NSDate.date().beginningOfMonth()
+    }
+    
+    class func endOfThisMonth() -> NSDate {
+        return NSDate.date().beginningOfMonth()
+    }
+    
+    class func dateMonthsFromNow(months: Int) -> NSDate {
+        return NSDate.date().addMonths(months)
+    }
+    
+    func beginningOfMonth() -> NSDate {
+        let calendar = NSCalendar.currentCalendar()
+        
+        let components = calendar.components(.YearCalendarUnit | .MonthCalendarUnit, fromDate: self)
+        
+        return calendar.dateFromComponents(components)!
+    }
+    
+    func endOfMonth() -> NSDate {
+        let calendar = NSCalendar.currentCalendar()
+        
+        let components = NSDateComponents()
+        components.month = 1
+        
+        return calendar.dateByAddingComponents(components, toDate: self.self.beginningOfMonth(), options: nil)!.dateByAddingTimeInterval(-1)
+    }
+    
+    func addMonths(months: Int) -> NSDate {
+        let calendar = NSCalendar.currentCalendar()
+        
+        let components = NSDateComponents()
+        components.month = months
+        
+        return calendar.dateByAddingComponents(components, toDate: self, options: nil)!
+    }
+    
+    // MARK: Years
+    
+    class func beginningOfThisYear() -> NSDate {
+        return NSDate.date().beginningOfYear()
+    }
+    
+    class func endOfThisYear() -> NSDate {
+        return NSDate.date().endOfYear()
+    }
+    
+    class func dateYearsFromNow(years: Int) -> NSDate {
+        return NSDate.date().addYears(years)
+    }
+    
+    func beginningOfYear() -> NSDate {
+        let calendar = NSCalendar.currentCalendar()
+        
+        let components = calendar.components(.YearCalendarUnit, fromDate: self)
+        
+        return calendar.dateFromComponents(components)!
+    }
+    
+    func endOfYear() -> NSDate {
+        let calendar = NSCalendar.currentCalendar()
+        
+        let components = NSDateComponents()
+        components.year = 1
+        
+        return calendar.dateByAddingComponents(components, toDate: self.self.beginningOfYear(), options: nil)!.dateByAddingTimeInterval(-1)
+    }
+    
+    func addYears(years: Int) -> NSDate {
+        let calendar = NSCalendar.currentCalendar()
+        
+        let components = NSDateComponents()
+        components.year = years
+        
+        return calendar.dateByAddingComponents(components, toDate: self, options: nil)!
+    }
+}
---
+++ Steps/StepCell.swift
@@ -0,0 +1,89 @@
+//
+//  StepCell.swift
+//  Steps
+//
+//  Created by Adam Binsz on 8/29/14.
+//  Copyright (c) 2014 Adam Binsz. All rights reserved.
+//
+
+import UIKit
+
+class StepCell: UITableViewCell {
+    
+    var countLabel: UICountingLabel
+    var dayLabel: UILabel
+    private var barView: UIView
+    // A value between 0.0 and 1.0
+    private var barWidth: CGFloat = 0.0
+    
+    func setBarWidth(width: CGFloat, animated: Bool) {
+        barWidth = width
+        var f = bounds
+        f.size.width *= barWidth
+        f.size.width = f.size.width < 8 ? 8 : f.size.width
+        if animated {
+            var b = bounds
+            b.size.width = 8
+            barView.frame = b
+            
+            UIView.animateWithDuration(10.0 / 9.0, delay: 0.1, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.0, options: UIViewAnimationOptions.BeginFromCurrentState, animations: { () -> Void in
+                self.barView.frame = f
+            }, completion: nil)
+        } else {
+            barView.frame = f
+        }
+    }
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String!) {
+        barView = UIView()
+        barView.backgroundColor = UIColor.whiteColor().colorWithAlphaComponent(0.10)
+        countLabel = UICountingLabel()
+        countLabel.text = "0"
+        countLabel.formatBlock = {(floatValue: Float) -> String in
+            return Int(floatValue).formattedString()
+        }
+        dayLabel = UILabel()
+        
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        
+        contentView.addSubview(barView)
+        
+        let labels = [countLabel, dayLabel]
+        let yPadding: CGFloat = 16.0
+        for label in labels {
+            label.setTranslatesAutoresizingMaskIntoConstraints(false)
+            label.textColor = UIColor.whiteColor()
+            var labelFontSize: CGFloat = 24.0
+            if UIScreen.mainScreen().scale > 2.0 {
+                labelFontSize *= 1.2
+            }
+            label.font = UIFont(name: "HelveticaNeue-Light", size: labelFontSize)
+            contentView.addSubview(label)
+            
+            contentView.addConstraint(NSLayoutConstraint(item: label, attribute: .CenterY, relatedBy: .Equal, toItem: contentView, attribute: .CenterY, multiplier: 1.0, constant: 0.0))
+            contentView.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|-(\(yPadding))-[label]-(\(yPadding))-|", options: .DirectionLeadingToTrailing, metrics: nil, views: ["label" : label]))
+        }
+        
+//        countLabel.textAlignment = NSTextAlignment.Right
+//        dayLabel.textAlignment = NSTextAlignment.Left
+        
+        let xPadding: CGFloat = 10.0
+        contentView.addConstraint(NSLayoutConstraint(item: countLabel, attribute: .Right, relatedBy: .Equal, toItem: contentView, attribute: .Right, multiplier: 1.0, constant: -xPadding))
+        contentView.addConstraint(NSLayoutConstraint(item: dayLabel, attribute: .Left, relatedBy: .Equal, toItem: contentView, attribute: .Left, multiplier: 1.0, constant: xPadding))
+        
+    }
+
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        var f = barView.frame
+        f.size.height = CGRectGetHeight(bounds)
+        barView.frame = f
+    }
+
+}
