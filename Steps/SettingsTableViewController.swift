a98429013d80ac60e05a74020d1cafd14586a942
Adam Binsz <adam@adambinsz.com>
2016-08-21 19:16:13 -0400

    Reverted in app purchase changes

--- Steps/SettingsTableViewController.swift
+++ Steps/SettingsTableViewController.swift
@@ -7,46 +7,16 @@
 //
 
 import UIKit
-import StoreKit
 import BRYXGradientView
 
-enum SettingsTableViewSection: Int {
-    case Units, Upgrade
-}
-
-class SettingsTableViewController: UITableViewController, SKProductsRequestDelegate {
+class SettingsTableViewController: UITableViewController {
     
     @IBOutlet weak var unitSwitch: UISwitch!
-    
-    @IBOutlet weak var upgradeLabel: UILabel!
-    @IBOutlet weak var priceLabel: UILabel!
-    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
-    
-    static let numberFormatter: NSNumberFormatter = {
-        let numberFormatter = NSNumberFormatter()
-        numberFormatter.numberStyle = .CurrencyStyle
-        return numberFormatter
-    }()
-    
-    var upgradeProduct: SKProduct? {
-        didSet {
-            tableView.reloadData()
-            activityIndicator.stopAnimating()
-            
-            guard let upgradeProduct = upgradeProduct else { return }
-            upgradeLabel.text = upgradeProduct.localizedTitle
-            
-            SettingsTableViewController.numberFormatter.locale = upgradeProduct.priceLocale
-            priceLabel.text = SettingsTableViewController.numberFormatter.stringFromNumber(upgradeProduct.price)
-        }
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         unitSwitch.on = Settings.useMetric
-        
-        requestProductInfo()
     }
     
     @IBAction func unitSwitchChanged(sender: AnyObject) {
@@ -62,34 +32,5 @@ class SettingsTableViewController: UITableViewController, SKProductsRequestDeleg
         guard (indexPath.section, indexPath.row) == (1, 0) else { return }
 
     }
-    
-    override func tableView(tableView: UITableView, willSelectRowAtIndexPath indexPath: NSIndexPath) -> NSIndexPath? {
-        return upgradeProduct == nil ? nil : indexPath
-    }
-    
-    func requestProductInfo() {
-        guard SKPaymentQueue.canMakePayments() else {
-            print("Cannot make payments.")
-            return
-        }
-        
-        activityIndicator.startAnimating()
-        
-        let productIdentifier: Set<String> = ["com.adambinsz.Steps.Upgrade_Test"]
-        let productRequest = SKProductsRequest(productIdentifiers: productIdentifier)
-        productRequest.delegate = self
-        productRequest.start()
-    }
-    
-    // MARK: - SKProductsRequestDelegate methods
-    func productsRequest(request: SKProductsRequest, didReceiveResponse response: SKProductsResponse) {
-        upgradeProduct = response.products.first
-    }
-    
-    func request(request: SKRequest, didFailWithError error: NSError) {
-        print(error)
-    }
-    
-    
 }
 
